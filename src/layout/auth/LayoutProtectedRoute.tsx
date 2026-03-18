d62ebe2695757928ff87fed1cc79bfb4b9a79fc4
Carlos <rvjosecarlos@gmail.com>
2026-03-17 23:55:10 -0600

    fix: carga de datos en el store del usuario

--- src/layout/auth/LayoutProtectedRoute.tsx
+++ src/layout/auth/LayoutProtectedRoute.tsx
@@ -1,8 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "../../hooks/useUser";
+import { useUserStore } from "../../store/userStore";
 
 export default function ProtectedRoute(){
+    const { setUserData } = useUserStore();
     const { getUserFromStore, getUserFromStorage } = useUser();
-    if( getUserFromStore() || getUserFromStorage() ) return <Outlet/>
-    return <Navigate to="/auth/login" />;
+    if( !getUserFromStore() ) {
+        const userStorage = getUserFromStorage();
+        if( userStorage ){
+            setUserData(userStorage);
+        }
+        else{
+            return <Navigate to="/auth/login" />;
+        }
+    };
+    return <Outlet/>;
 };
