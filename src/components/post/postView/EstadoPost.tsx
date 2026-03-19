b13ee6c2a52d131915e75df3a32d96d89474bbaa
Nicole <nicolerosasvallejo@hotmail.com>
2026-03-18 19:52:01 -0600

    chore: seccion de estados con todos los estados

--- src/components/post/postView/EstadoPost.tsx
+++ src/components/post/postView/EstadoPost.tsx
@@ -12,7 +12,7 @@ const estadosDetails = {
         titulo: "Asignado"
     },
     "pendiente": {
-        color: "bg-amber-400",
+        color: "bg-yellow-400",
         titulo: "Pendiente"
     },
     "en progreso": {
@@ -20,7 +20,7 @@ const estadosDetails = {
         titulo: "En Progreso"
     },
     "resuelto": {
-        color: "bg-lime-400",
+        color: "bg-green-500",
         titulo: "Resuelto"
     }
 }
@@ -28,28 +28,37 @@ const estadosDetails = {
 export default function EstadoPost({ estados }: EstadoPostProps) {
     if( !estados || !estados.length ) return null;
 
-    const { estado, descripcion, createdAt, updatedAt, editado, alias: { alias } } = estados[ estados.length - 1];
-    
-    const fecha = editado ? updatedAt : createdAt;
-    const estadoDetail = estadosDetails[estado];
-
     return (
-        <Card className="border p-5 w-full">
-            <div className="w-full flex items-start gap-3">
-                <div className={`flex items-center justify-center h-7 w-7 shrink-0 rounded-full ${estadoDetail.color}`}>
-                    <div className="h-3 w-3 bg-card rounded-full"/>
-                </div>
+        <Card className="border p-5 w-full relative">
+            <div className="absolute top-6 bottom-5 left-[33px] w-0.5 bg-muted" />
+            
+            { estados.map( data => {
+                const { estadoId, estado, descripcion, createdAt, updatedAt, editado, alias: { alias } } = data;
+    
+                const fecha = editado ? updatedAt : createdAt;
+                const estadoDetail = estadosDetails[estado];
 
-                <div className="w-full">
-                    <div className="flex items-center justify-between">
-                        <p className="font-semibold">{estadoDetail.titulo}</p>
-                        <p className="text-xs text-muted-foreground">{formatDate(fecha)}</p> 
+                return (
+                    <div
+                        key={estadoId}
+                        className="w-full flex items-start gap-3"
+                    >                   
+                        <div className={`z-10 flex items-center justify-center h-7 w-7 shrink-0 rounded-full ${estadoDetail.color}`}>
+                            <div className="h-3 w-3 bg-card rounded-full"/>
+                        </div>              
+
+                        <div className="w-full">
+                            <div className="flex items-center justify-between">
+                                <p className="font-semibold">{estadoDetail.titulo}</p>
+                                <p className="text-xs text-muted-foreground">{formatDate(fecha)}</p> 
+                            </div>
+                            
+                            <p className="text-sm text-muted-foreground">{descripcion}</p>
+                            <p className="text-xs text-muted-foreground">Por: {alias}</p>
+                        </div>
                     </div>
-                    
-                    <p className="text-sm text-muted-foreground">{descripcion}</p>
-                    <p className="text-xs text-muted-foreground">Por: {alias}</p>
-                </div>
-            </div>
+                )
+            })}
         </Card>
     )
 }
