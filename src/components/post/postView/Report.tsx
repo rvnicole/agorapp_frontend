d5d4fb9a9362353390c4c4ee74e19b672de0d612
Nicole <nicolerosasvallejo@hotmail.com>
2026-04-02 15:51:38 -0600

    fix: carrusel de imagenes con blur y sin boton de editar

--- src/components/post/postView/Report.tsx
+++ src/components/post/postView/Report.tsx
@@ -3,8 +3,6 @@ import InformationPost from "./InformationPost";
 import UbicacionPost from "./UbicacionPost";
 import EstadoPost from "./EstadoPost";
 import CommentsPost from "./comments/CommentsPost";
-import { Button } from "../../ui/Button";
-import { Images } from "lucide-react";
 import type { PostRespuesta } from "../../../types";
 
 type ReportProps = {
@@ -14,16 +12,7 @@ type ReportProps = {
 export default function Report({ post }: ReportProps) {
     return (
         <div className="flex flex-col gap-5 items-center justify-center w-3xl">
-            { post.imagenes && 
-                <div className="relative">
-                    <Button variant="secondary" className="absolute top-2 right-2 z-10 flex items-center gap-1"> 
-                        <Images className="h-5 w-5"/>
-                        Editar
-                    </Button>
-
-                    <CarruselImgs imgs={post.imagenes} /> 
-                </div>
-            }
+            { post.imagenes && <CarruselImgs imgs={post.imagenes} /> }
 
             <InformationPost post={post} />
 
