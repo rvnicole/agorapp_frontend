62522979885d722b8bc1861f7c34eef7ef474453
Nicole <nicolerosasvallejo@hotmail.com>
2026-04-07 18:50:31 -0600

    add: delete post popover

--- src/components/post/postDelete/DeletePost.tsx
+++ src/components/post/postDelete/DeletePost.tsx
@@ -1,9 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
 import { useMessageStore } from "../../../store/messageStore";
+import { useNavigate } from "react-router-dom";
 import { useUserStore } from "../../../store/userStore";
 import { Button } from "../../ui/Button";
-import { Loader2, Trash2 } from "lucide-react";
 import { deletePost } from "../../../api/PostAPI";
+import { Popover, PopoverContent, PopoverItem, PopoverTrigger } from "../../ui/Popover";
+import { EllipsisVertical, Trash2 } from "lucide-react";
 import type { ApiErrorType, PostRespuesta } from "../../../types";
 
 type DeletePostProps = {
@@ -15,13 +17,15 @@ type DeletePostProps = {
 export default function DeletePost({ id, createdAt, creador }: DeletePostProps) {
     const { showMessages } = useMessageStore( state => state );
     const { user: { alias }} = useUserStore( state => state );
+    const navigate = useNavigate();
 
     if( alias !== creador ) return null;
 
     const { mutate, isPending } = useMutation({
         mutationFn: deletePost,
         onSuccess: () => {
-            showMessages("success", "Reporte creado");
+            navigate("/");
+            showMessages("success", "Reporte eliminado");
         },
         onError: (error: ApiErrorType) => {
             error.messages.forEach((error: string) => showMessages("error", error)); 
@@ -30,24 +34,30 @@ export default function DeletePost({ id, createdAt, creador }: DeletePostProps) 
 
     return (
         <div>
-            { isPending ?
-                <div className="flex items-center gap-1.5">
-                    <Loader2 className="h-4 w-4 animate-spin text-destructive" />
-                    <p className="font-semibold text-destructive">
-                        Eliminando
-                        <span className="animate-pulse">...</span>
-                    </p>
-                </div>
-                :
-                <Button
-                    size="icon-lg"
-                    variant="destructive"
-                    className="flex justify-center items-center rounded-full"
-                    onClick={() => mutate({ id, createdAt })}
-                >
-                    <Trash2 />
-                </Button>
-            }
+            <Popover>
+                <PopoverTrigger>
+                    <Button 
+                        className="flex items-center justify-center rounded-full bg-transparent"
+                        variant="secondary"
+                        size="icon"
+                        disabled={isPending}
+                    >
+                        <EllipsisVertical className="h-5 w-5 text-muted-foreground"/>
+                    </Button>
+                </PopoverTrigger>
+
+                { !isPending &&
+                    <PopoverContent className="w-fit">
+                        <PopoverItem 
+                            key={`delete-post-${id}`}
+                            onClick={() => mutate({ id, createdAt })}
+                        >
+                            <Trash2 className="size-4"/>
+                            Eliminar
+                        </PopoverItem>
+                    </PopoverContent>
+                }
+            </Popover>
         </div>
     )
 }
